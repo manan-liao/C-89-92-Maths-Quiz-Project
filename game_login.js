22c5fa597373c24dd2c69b25ec2940672bacc975
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-05-04 17:36:02 +1000

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,11 +1,11 @@
 function addUser() {
     //getting the player names from the screen
-    number1 = document.getElementById("number1_input").value;
-    number2 = document.getElementById("number2_input").value;
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
     
     //saving the player names in the local storage
-    localStorage.setItem("number1", number1);
-    localStorage.setItem("number2", number2);
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
     
     //redirecting to the games page
     window.location = "game_page.html";
